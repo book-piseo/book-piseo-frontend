39d5ab4532e5ffc4e67d8f0c79bd5a2c35d12772
donghee <donghee@donghees-MacBook-Pro.local>
2023-09-03 16:08:44 +0900

    feat: 작성 확인 모달 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,6 +51,20 @@ const config: Config = {
 			p2_medium: ['0.875rem', { fontWeight: '500' }],
 			p2_regular: ['0.875rem', { fontWeight: '400' }],
 		},
+		keyframes: {
+			fadeIn: {
+				'0%': { opacity: '0' },
+				'100%': { opacity: '1' },
+			},
+			fadeOut: {
+				'0%': { opacity: '1' },
+				'100%': { opacity: '0' },
+			},
+		},
+		animation: {
+			fadeIn: 'fadeIn 0.3s ease-in-out',
+			fadeOut: 'fadeOut 0.3s ease-in-out',
+		},
 		extend: {},
 	},
 	plugins: [require('tailwindcss-convert-px-to-rem')],
