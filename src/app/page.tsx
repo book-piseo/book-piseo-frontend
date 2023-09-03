09a15f7f9adfaa26868d1361ae5b721480ca655f
donghee <donghee@donghees-MacBook-Pro.local>
2023-09-03 16:11:34 +0900

    chore: prettier 적용 및 package 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-'use client'
-import { useAuthStore } from '@/stores/useAuthStore'
-import Header from '@components/layout/Header'
-import useStore from '@hooks/useStore'
+'use client';
+import { useAuthStore } from '@/stores/useAuthStore';
+import Header from '@components/layout/Header';
+import useStore from '@hooks/useStore';
 
 export default function RootPage({ children }: { children: React.ReactNode }) {
-  // MARK :: zustand persist 적용 시, Error 발생
-  const store = useStore(useAuthStore, (state) => state)
-  console.log({ store })
+	// MARK :: zustand persist 적용 시, Error 발생
+	const store = useStore(useAuthStore, (state) => state);
 
-  return (
-    <div className="w-screen h-full">
-      {store?.isLogin && <Header />}
-      <main>{children}</main>
-    </div>
-  )
+	return (
+		<div className="w-screen h-full">
+			{/* {store?.isLogin && <Header />} */}
+			<Header />
+			<main>{children}</main>
+		</div>
+	);
 }
