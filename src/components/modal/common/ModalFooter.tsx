bef7babdfe8b3d9376b4c9441686ec996945e760
donghee <donghee@donghees-MacBook-Pro.local>
2023-09-04 00:40:24 +0900

    feat: modal component 수정

--- src/components/modal/common/ModalFooter.tsx
+++ src/components/modal/common/ModalFooter.tsx
@@ -1,16 +1,15 @@
-import { useModalStore } from '@/stores/useModalStore'
-import RoundButton from '@components/elements/buttons/RoundButton'
+import RoundButton from '@components/elements/buttons/RoundButton';
 
 export type Props = {
-	onConfirm: () => void
-}
+	onCancel: () => void;
+	onConfirm: () => void;
+};
 
-export const ModalFooter = ({ onConfirm }: Props) => {
-	const { closeModal } = useModalStore()
+export const ModalFooter = ({ onCancel, onConfirm }: Props) => {
 	return (
 		<div className="flex items-center rem:gap-[16px] justify-center rem:py-[20px]">
-			<RoundButton type="secondary" label="취소" onClick={closeModal} />
+			<RoundButton type="secondary" label="취소" onClick={onCancel} />
 			<RoundButton type="primary" label="확인" onClick={onConfirm} />
 		</div>
-	)
-}
+	);
+};
