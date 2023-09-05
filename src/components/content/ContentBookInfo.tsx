b36fda4b0813a37cd8f9c78bf9c82685662c6c80
donghee <donghee@donghees-MacBook-Pro.local>
2023-09-06 01:30:43 +0900

    feat: content page api 연동 세팅

--- src/components/content/ContentBookInfo.tsx
+++ src/components/content/ContentBookInfo.tsx
@@ -1,30 +1,49 @@
+'use client';
 import BookCoverWithNaverLink from './BookCoverWithNaverLink';
-import DummyBookCoverImage from '../../../public/assets/images/img_book-cover-example.png';
+import { ContentsInfoResponse } from '@models/contents.model';
+import Image from 'next/image';
 
-export const ContentBookInfo = () => {
+export const ContentBookInfo = ({ content }: { content: Partial<ContentsInfoResponse> }) => {
+	const { bookInfo } = content;
+
+	const getRegDt = (content: Partial<ContentsInfoResponse>) => {
+		if (content && content.regDt) {
+			return content.regDt.substring(0, content.regDt.indexOf('T'));
+		}
+		return '';
+	};
+
 	return (
 		<div className="flex rem:gap-[40px] w-full">
 			{/* 책 표지 */}
-			<BookCoverWithNaverLink src={DummyBookCoverImage} onClickLink={() => {}} />
+			<BookCoverWithNaverLink src={bookInfo?.image || ''} link={bookInfo?.link || ''} />
 			{/* 책 설명  */}
 			<div className="flex flex-col justify-between w-full rem:h-[430px]">
 				<div className="flex flex-col rem:gap-[24px]">
-					<div className="w-full text-h1_bold">
-						일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십
+					<div className="w-full text-h1_bold text-ellipsis-line-3">{content.contentsTitle}</div>
+					<div className="flex items-center text-p1_regular text-dark-grey-2 rem:gap-[14px]">
+						<div className="relative rem:w-[32px] rem:h-[32px]">
+							<Image
+								src={content.writerInfo?.profileImg || ''}
+								alt="profile"
+								className="rounded-full"
+								unoptimized
+								fill
+							/>
+						</div>
+						<span>{content.writerInfo?.userName}</span>
+						<span>|</span>
+						<span> {getRegDt(content)}</span>
 					</div>
-					<div className="text-p1_regular text-dark-grey-2">책을 피서 운영자 | YYYY.MM.DD</div>
 				</div>
 
 				<div className="bg-primary-sub w-full rem:py-[24px] rem:pr-[40px] rem:pl-[30px] rounded-[10px]">
-					<div className="text-s1_semibold">1984</div>
+					<div className="text-s1_semibold">{bookInfo?.title}</div>
 					<div className="flex rem:gap-[10px] text-s3_medium text-dark-grey-1 text-ellipsis-line-1">
 						<span>저자</span>
-						<span>조지 오웰</span>
-						<span>・</span>
-						<span>번역</span>
-						<span>김승욱</span>
+						<span>{bookInfo?.author}</span>
 					</div>
-					<div className="text-dark-grey-2 text-s3_medium">문예 출판사</div>
+					<div className="text-dark-grey-2 text-s3_medium">{bookInfo?.publisher}</div>
 				</div>
 			</div>
 		</div>
