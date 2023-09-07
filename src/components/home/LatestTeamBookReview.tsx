53d697e5442ed707393bfd1ffccf0f8492ef880f
im-wen3y <imwen3y@gmail.com>
2023-09-07 21:11:36 +0900

    feat: 팀원이 남긴 독서 기록 > 컨텐츠 페이지로 이동 클릭 이벤트 추가

--- src/components/home/LatestTeamBookReview.tsx
+++ src/components/home/LatestTeamBookReview.tsx
@@ -2,18 +2,19 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { IcArrowUpLight, IcBook, IcTeamHistoryCheck } from '@assets/icons';
-import { ImgBookCoverExample, ImgEmptyBookCover } from '@assets/images';
+import { ImgEmptyBookCover } from '@assets/images';
 import { LinkButton } from '@components/elements/buttons/LinkButton';
 import AvatarProfile from '@components/common/AvatarProfile';
 import { format } from 'date-fns';
 import { ContentsInfoResponse } from '@models/contents.model';
-import EmptyBookCoverImage from '@components/common/EmptyBookCoverImage';
+import { useRouter } from 'next/navigation';
 
 type LatestTeamBookReviewProps = {
 	teamBookReviews?: ContentsInfoResponse[];
 };
 
 const LatestTeamBookReview = ({ teamBookReviews }: LatestTeamBookReviewProps) => {
+	const router = useRouter();
 	const [firstBookReview, setFirstBookReview] = useState<ContentsInfoResponse | null>(null);
 	const regDate = firstBookReview ? new Date(firstBookReview?.regDt) : new Date();
 
@@ -24,6 +25,10 @@ const LatestTeamBookReview = ({ teamBookReviews }: LatestTeamBookReviewProps) =>
 
 		// eslint-disable-next-line
 	}, [JSON.stringify(teamBookReviews)]);
+
+	const handleGoToContentsPage = (contentId: string) => {
+		router.push(`/content?id=${contentId}`);
+	};
 
 	return (
 		<>
@@ -43,7 +48,10 @@ const LatestTeamBookReview = ({ teamBookReviews }: LatestTeamBookReviewProps) =>
 						</div> */}
 					</div>
 					<div className="grid grid-cols-[30%_70%]">
-						<div className="relative rem:min-w-[305px] w-full rem:h-[270px]">
+						<div
+							className="relative rem:min-w-[305px] w-full rem:h-[270px] cursor-pointer"
+							onClick={() => handleGoToContentsPage(firstBookReview?.contentsId || '')}
+						>
 							<Image
 								fill
 								unoptimized
@@ -64,7 +72,10 @@ const LatestTeamBookReview = ({ teamBookReviews }: LatestTeamBookReviewProps) =>
 										<Image src={IcArrowUpLight} alt="전체 보기" className="rem:w-[22px] rem:h-[22px]" />
 									</LinkButton>
 								</div>
-								<div className="flex flex-col rem:gap-[14px]">
+								<div
+									className="flex flex-col rem:gap-[14px] cursor-pointer"
+									onClick={() => handleGoToContentsPage(firstBookReview?.contentsId || '')}
+								>
 									<div className="text-dark-grey-1 text-s1_semibold text-ellipsis-line-2 rem:leading-[28px]">
 										{firstBookReview?.contentsTitle || ''}
 									</div>
@@ -80,7 +91,10 @@ const LatestTeamBookReview = ({ teamBookReviews }: LatestTeamBookReviewProps) =>
 									</div>
 								</div>
 							</div>
-							<div className="flex max-w-[80%] w-fit items-center rem:p-[10px] rem:gap-[8px] bg-white rounded-[4px]">
+							<div
+								className="flex max-w-[80%] w-fit items-center rem:p-[10px] rem:gap-[8px] bg-white rounded-[4px] cursor-pointer"
+								onClick={() => handleGoToContentsPage(firstBookReview?.contentsId || '')}
+							>
 								<Image src={IcBook} alt="책제목" className="rem:w-[24px] rem:h-[24px]" />
 								<p className="text-ellipsis-line-1 text-dark-grey-1 text-p1_semibold">
 									{firstBookReview?.bookInfo?.title || ''}
